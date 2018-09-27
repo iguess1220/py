cdebffa931c5afb6d4034c2854e6d048728f1c0e
cy <17710890916@163.com>
2018-09-27 18:28:46 +0800

    0926

--- module/test.py
+++ module/test.py
@@ -1,3 +1,14 @@
+__all__ = ["Count",'haha']  # 当from xx import * 时 通配符选定的为__all__的内容
+
 def haha():
     print("hello,world")
 Count = 10
+
+class Dog:
+    pass
+
+
+if __name__ == '__main__':
+    """当__name__在所在自己文件中被访问时为__main__ ,当被作为模块导入其他文件时，在其他文件中为自己的文件名称"""
+    print("测试")
+    haha()
--- module/test2.py
+++ module/test2.py
@@ -1,4 +1,13 @@
 # 可直接导入文件名，没后缀， 可使用函数和全局变量
-import test
-test.haha()
-print(test.Count)
+# import test
+# test.haha()
+# print(test.Count)
+
+
+# from test import haha,Count,Dog
+# haha()
+# print(Count)
+
+from test import *  # 当导入* 或者直接import 文件名时，会先执行一遍该文件
+haha()
+#
